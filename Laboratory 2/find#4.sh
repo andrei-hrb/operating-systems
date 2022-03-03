e28cba3e2fc10bd8ddc14386fbdebef90e7a2a90
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-03-03 19:42:09 +0200

    Update find#4.sh

--- Laboratory 2/find#4.sh
+++ Laboratory 2/find#4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-find ${0} -perm /u=x -exec stat -c "%x %s\/1024kb %n" {} \;
+find $1 -perm /u=x -exec stat -c "%x %s\/1024kb %n" {} \;
 
-# ${0}			argumentul primit
+# $1      			argumentul primit
 # -perm /u=x		doar fisierele pe care utilizatorul curent (u) are dreptul sa le exeucte (x)
 # %s\/1024kb		dimensiunea in kbytes, teoretic corect, practic nu
 
