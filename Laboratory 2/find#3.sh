9d0fbbd23bca1a667e75b6b7abacbdbd9fb25208
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-03-03 19:13:25 +0200

    added lab 2 partly

---
+++ Laboratory 2/find#3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+find ~ -mtime +7 -type f -exec stat -c "%A %x %n" {} \;
+
+# -mtime +7          	fisierele modificate in ultima sapatamana
+# -type f 		fisiere normale
+# -exec 		fisierele gasite vor fi trecute prin comanda care urmeaza dupa exec
+# -c "%A %x %n"		formatul afisarii
+# %A			drepturile de access ale fisierului
+# %x			data ultimei accesari ale fisierului
+# %n 			numele fisierului
---
+++ Laboratory 2/sort#3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sort -n -t: -k 3 /etc/group
+
+# -n         sortare numerica
+# -t:        delimitator de coloane
+# -k 3       coloana dupa care vrem sa sortam
