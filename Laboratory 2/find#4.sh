ac1b990d16db28799faaaf738116dcfb97dc1f81
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-03-03 19:42:27 +0200

    Update find#4.sh

--- Laboratory 2/find#4.sh
+++ Laboratory 2/find#4.sh
@@ -2,7 +2,7 @@
 
 find $1 -perm /u=x -exec stat -c "%x %s\/1024kb %n" {} \;
 
-# $1      			argumentul primit
-# -perm /u=x		doar fisierele pe care utilizatorul curent (u) are dreptul sa le exeucte (x)
-# %s\/1024kb		dimensiunea in kbytes, teoretic corect, practic nu
+# $1            argumentul primit
+# -perm /u=x    doar fisierele pe care utilizatorul curent (u) are dreptul sa le exeucte (x)
+# %s\/1024kb    dimensiunea in kbytes, teoretic corect, practic nu
 
